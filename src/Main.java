c79045b27b5c8cf730e3e80c8f1fb0ebe44fdca3
artjom <artemdobrom@gmail.com>
2023-08-10 23:24:55 +0300

    Курсовая готова

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,39 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner dialog = new Scanner(System.in);
+        System.out.println("Напишите, что Вы хотите проверить на палиндром");
+        String checkForPalindorome = dialog.nextLine();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        checkForPalindorome = checkForPalindorome.replace(",", "");
+        checkForPalindorome = checkForPalindorome.replace(" ", "");
+        checkForPalindorome = checkForPalindorome.replace(".", "");
+        checkForPalindorome = checkForPalindorome.replace("-", "");
+        checkForPalindorome = checkForPalindorome.replace("=", "");
+        checkForPalindorome = checkForPalindorome.replace("+", "");
+        checkForPalindorome = checkForPalindorome.replace("_", "");
+
+        System.out.println("проверка строчки: "+checkForPalindorome);
+        System.out.println();
+        for (int i = 0; i < checkForPalindorome.length()/2; i++) {
+            System.out.print(checkForPalindorome.charAt(i) + "-" + checkForPalindorome.charAt(checkForPalindorome.length() - i - 1));
+
+            if (checkForPalindorome.charAt(i) == checkForPalindorome.charAt(checkForPalindorome.length() - i - 1)) {
+                System.out.println(" -эта пара палиндром");
+
+            } else if (checkForPalindorome.charAt(i) != checkForPalindorome.charAt(checkForPalindorome.length() - i - 1)) {
+
+                System.out.println(" -эта пара не палиндром");
+            }
+            }
+            }
         }
-    }
-}
+
+
+
+
+
